93ec33f1d59769fbcdda18c0e3b067536e8b2392
Smilerproxz <131507750+Smilerproxz@users.noreply.github.com>
2023-10-20 23:07:05 -0400

    Add files via upload

---
+++ js/phaser.min.js
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>
+<html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>
+<html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>
+<html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+ <!--<![endif]-->
+ <head>
+  <meta charset="utf-8"/>
+  <meta content="IE=edge" http-equiv="X-UA-Compatible"/>
+  <meta content="" name="description"/>
+  <meta content="width=480, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.01, user-scalable=no" name="viewport"/>
+  <title>
+   Pick a Lock
+  </title>
+  <link href="css/normalize.css" rel="stylesheet"/>
+  <link href="css/game.css" rel="stylesheet"/>
+  <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+  <link href="assets/favicon/icon-36x36.png" rel="icon" sizes="16x16" type="image/png"/>
+  <link href="assets/favicon/icon-48x48.png" rel="icon" sizes="32x32" type="image/png"/>
+  <link href="assets/favicon/icon-72x72.png" rel="icon" sizes="72x72" type="image/png"/>
+  <link href="assets/favicon/icon-36x36.png" rel="apple-touch-icon" sizes="16x16" type="image/png"/>
+  <link href="assets/favicon/icon-48x48.png" rel="apple-touch-icon" sizes="32x32" type="image/png"/>
+  <link href="assets/favicon/icon-72x72.png" rel="apple-touch-icon" sizes="72x72" type="image/png"/>
+  <link href="assets/favicon/icon-152x152.png" rel="apple-touch-icon" sizes="152x152" type="image/png"/>
+  <link href="assets/favicon/icon-512x512.png" rel="apple-touch-icon" sizes="512x512" type="image/png"/>
+  <script src="/sdk/modules/middleware/gamepix.middleware.js">
+  </script>
+  <style type="text/css">
+   body { background: transparent !important; }
+   .gpxForce-bckgr-gamefrm { background-image: url("https://games.assets.gamepix.com/40317/cover/medium.png"), url("https://games.assets.gamepix.com/40317/thumbnail/small.png") !important; width: 100%; height: 100%; filter: blur(20px); -webkit-filter: blur(20px); backdrop-filter: blur(20px); -webkit-backdrop-filter: blur(20px); background-position: 50% !important; background-repeat: no-repeat !important; background-size: cover !important; position: absolute; top: 0; left: 0; -webkit-transform: scale(1.3); transform: scale(1.3); z-index: -1; opacity: 1;}
+  </style>
+ </head>
+ <body style="background-color: #000000">
+  <div id="wrongRotation">
+   <img alt="" src="assets/img_480/rotate_screen.png"/>
+  </div>
+  <div id="game">
+   <script src="js/phaser.min.js">
+   </script>
+   <script src="js/pl.js">
+   </script>
+  </div>
+  <div class="gpxForce-bckgr-gamefrm">
+  </div>
+ </body>
+</html>
